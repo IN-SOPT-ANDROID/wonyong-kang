10798fecc527b5ec5bda545850f21539bd38dab9
KWY0218 <wodrk1001@gmail.com>
2022-10-03 02:42:11 +0900

    [ui] main 뷰 구현

---
+++ app/src/main/java/org/sopt/sample/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.sample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.sopt.sample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
