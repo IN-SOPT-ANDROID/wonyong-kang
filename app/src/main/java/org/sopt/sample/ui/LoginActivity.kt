b5921a9db078f2d6f5aeedf7a7f50ac4b41bda02
KWY0218 <wodrk1001@gmail.com>
2022-10-03 02:38:46 +0900

    [fix] 네이밍 수정

--- app/src/main/java/org/sopt/sample/ui/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/ui/LoginActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.sample.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
