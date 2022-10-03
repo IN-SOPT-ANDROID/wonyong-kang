16ad059c1ac3cdd3b3234b30c67c8e825f77dc79
KWY0218 <wodrk1001@gmail.com>
2022-10-04 02:23:11 +0900

    [ui] sopt 버튼 구현

---
+++ compose/src/main/java/com/example/compose/presentation/login/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.example.compose.presentation.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+    }
+}
