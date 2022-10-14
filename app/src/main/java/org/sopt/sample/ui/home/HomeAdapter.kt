855f827f0294fb122cb3fff64f32e738fd75513d
KWY0218 <wodrk1001@gmail.com>
2022-10-14 13:19:14 +0900

    [feat] list adapter 기능 구현

---
+++ app/src/main/java/org/sopt/sample/ui/home/HomeAdapter.kt
@@ -0,0 +1,49 @@
+package org.sopt.sample.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.data.entity.Follower
+import org.sopt.sample.R
+import org.sopt.sample.databinding.ItemFollowerBinding
+
+class HomeAdapter : ListAdapter<Follower, HomeAdapter.FollowerViewHolder>(followerComparator) {
+    private lateinit var inflater: LayoutInflater
+
+    class FollowerViewHolder(
+        private val binding: ItemFollowerBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(follower: Follower) {
+            binding.tvFollowerName.text = follower.name
+            binding.ivFollowerProfile.load(follower.avatar) {
+                placeholder(R.drawable.ic_person)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+        if (!::inflater.isInitialized) inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFollowerBinding.inflate(inflater, parent, false)
+        return FollowerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current)
+    }
+
+    companion object {
+        private val followerComparator = object : DiffUtil.ItemCallback<Follower>() {
+            override fun areItemsTheSame(oldItem: Follower, newItem: Follower): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Follower, newItem: Follower): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
--- app/src/main/java/org/sopt/sample/ui/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/ui/home/HomeFragment.kt
@@ -5,15 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.sopt.sample.R
+import org.sopt.sample.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = requireNotNull(_binding) { "${this.javaClass.name} binding error" }
+    private lateinit var homeAdapter: HomeAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        homeAdapter = HomeAdapter()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        binding.rvHome.adapter = homeAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
