300646bd3d45b7b1b407bf799040da83feb115b5
KWY0218 <wodrk1001@gmail.com>
2022-10-21 01:20:50 +0900

    [feat] follower list ui 및 기능 구현
    coil dependency 사용해서 이미지 호출

--- compose/src/main/java/com/example/compose/presentation/home/HomeScreen.kt
+++ compose/src/main/java/com/example/compose/presentation/home/HomeScreen.kt
@@ -1,11 +1,97 @@
 package com.example.compose.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
+import com.example.compose.R
 import com.example.compose.navigation.MainContentNavGraph
+import com.example.data.entity.Follower
 import com.ramcosta.composedestinations.annotation.Destination
 
 @MainContentNavGraph(start = true)
 @Destination("home")
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    homeViewModel: HomeViewModel = hiltViewModel()
+) {
+    val followers by homeViewModel.followers.collectAsState()
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                text = stringResource(R.string.home_title),
+                style = MaterialTheme.typography.h6,
+                textAlign = TextAlign.Center
+            )
+        }
+        items(followers) { follower ->
+            FollowerItem(follower = follower)
+        }
+    }
+}
+
+@Composable
+private fun FollowerItem(follower: Follower) {
+    Surface(
+        elevation = 8.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 32.dp, vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            AsyncImage(
+                model = follower.avatar,
+                contentDescription = null,
+                placeholder = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentScale = ContentScale.Crop,
+                fallback = painterResource(id = R.drawable.ic_launcher_foreground),
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                text = follower.name,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
 }
