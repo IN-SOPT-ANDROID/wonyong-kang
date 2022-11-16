1b3dbbed20c01c7c36e8981ea9f78887344843bf
KWY0218 <wodrk1001@gmail.com>
2022-11-17 00:13:04 +0900

    [fix] annotation 수정

--- app/src/main/java/org/sopt/sample/di/ServiceModule.kt
+++ app/src/main/java/org/sopt/sample/di/ServiceModule.kt
@@ -6,19 +6,21 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
 import javax.inject.Singleton
+import org.sopt.sample.di.type.RetrofitType.REQ_RES
+import org.sopt.sample.di.type.RetrofitType.SOPT
+import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
     @Provides
     @Singleton
-    fun providesAuthService(@SoptRetrofit retrofit: Retrofit): AuthService =
+    fun providesAuthService(@Retrofit2(SOPT) retrofit: Retrofit): AuthService =
         retrofit.create(AuthService::class.java)
 
     @Provides
     @Singleton
-    fun providesMainService(@ReqResRetrofit retrofit: Retrofit): MainService =
+    fun providesMainService(@Retrofit2(REQ_RES) retrofit: Retrofit): MainService =
         retrofit.create(MainService::class.java)
 }
